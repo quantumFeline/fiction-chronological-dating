c09c484654c3d1b866da960bbebbc609e1a7cbad
Quantum Cat <veronika.netch@gmail.com>
2022-10-14 15:39:40 +0200

    spacy experiments

---
+++ TextPrerocessor.py
@@ -0,0 +1,31 @@
+import string
+import io
+
+import spacy
+
+
+class TextPreprocessor:
+
+    def __init__(self, file: object = None, filename: string = "input.txt"):
+        if file:
+            self.file = file
+        else:
+            self.file = open(filename, "r")
+
+    def __del__(self):
+        self.file.close()
+
+    def process(self):
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(self.file.read())
+        for token in doc:
+            print(token.text, token.pos_, token.dep_)
+
+
+if __name__ == "__main__":
+    with io.StringIO() as f:
+        f.write("Short text. Second sentence in the text.")
+        f.seek(0)
+        # print(f.read())
+        preprocessor = TextPreprocessor(f)
+        preprocessor.process()
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import json
 
-SLAVIC_LANGUAGES = ["Belarusian", "Bulgarian", "Czech",  "Macedonian","Old Church Slavonic", "Polish", "Russian",
+SLAVIC_LANGUAGES = ["Belarusian", "Bulgarian", "Czech",  "Macedonian", "Old Church Slavonic", "Polish", "Russian",
                     "Serbo-Croatian", "Slovak", "Slovene", "Ukrainian"]
 
 if __name__ == '__main__':
+    output = open("translations.txt", "w")
     with open('raw-wiktextract-data.json', 'r') as f:
         data = []
-        for i in range(1000):
+        for i in range(100000):
             obj = json.loads(f.readline())
             # print(json.dumps(obj, indent=2, sort_keys=True))
             if 'lang' in obj:
-                if obj['lang'] == "English":
+                if obj['lang'] != "Translingual":
                     data.append(obj)
                 else:
                     pass
@@ -18,10 +19,10 @@ if __name__ == '__main__':
         for obj in data:
             # print(json.dumps(obj, indent=2))
             if 'translations' in obj:
-                print(obj["word"])
+                print(obj["word"], file=output)
                 for tr in obj['translations']:
                     try:
                         if tr["lang"] in SLAVIC_LANGUAGES:
-                            print("\t", tr["lang"], tr["word"])
+                            print("\t", tr["lang"], tr["word"], file=output)
                     except KeyError:
-                        print("Error. ", tr)
+                        print("Error. ", tr, file=output)
