caae74170a962b4b83cbed86f9f2b660dfdf597e
Quantum Cat <veronika.netch@gmail.com>
2022-10-31 10:19:32 +0100

    playing with SAX reader

---
+++ XML_Reader.py
@@ -0,0 +1,36 @@
+import xml.sax as sax
+import xml.etree.ElementTree as ET
+
+
+def read_xml():
+    parser = ET.XMLPullParser(['start', 'end'])
+    i = 0
+    with open('enwikisource-20221020-pages-articles-multistream.xml', "rb") as f:
+        while True:
+            line = f.readline()
+            #print(line)
+            parser.feed(line)
+            for event, elem in parser.read_events():
+                print(event)
+                print(elem.tag, 'text=', elem.text)
+                print("\n\n")
+            i += 1
+            if i > 1000:
+                return
+    # context = ET.iterparse('enwikisource-20221020-pages-articles-multistream.xml', events=("start", "end"))
+    #
+    # for index, (event, elem) in enumerate(context):
+    #     print(index, event, elem)
+    #     if elem.tag == "text":
+    #         print("Text!")
+    #     # # Get the root element.
+    #     # if index == 0:
+    #     #     root = elem
+    #     #     print(root)
+    #     # if event == "end" and elem.tag == "record":
+    #     #     # ... process record elements ...
+    #     #     root.clear()
+
+
+if __name__ == "__main__":
+    read_xml()
